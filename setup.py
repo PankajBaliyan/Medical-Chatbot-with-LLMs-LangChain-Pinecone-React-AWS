616d7854c51a0f9220042c9ddf76f9d0b5cfb533
Pankaj Kumar <pankajbaliyan90@gmail.com>
2025-10-03 22:43:25 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical chatbot",
+    version="0.1.0",
+    author="Pankaj Kumar",
+    author_email="pankajbaliyan90@gmail.com",
+    description="A medical chatbot application",
+    packages=find_packages(),
+    install_requires=[],
+)
